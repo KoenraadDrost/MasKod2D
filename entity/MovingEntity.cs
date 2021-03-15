eb1fb11ff2eb3c8674196c65b168a3e18bbd7884
MartinSteenb <33652339+MartinSteenb@users.noreply.github.com>
2021-03-15 10:44:29 +0100

    First commit

---
+++ entity/MovingEntity.cs
@@ -0,0 +1,55 @@
+﻿using SteeringCS.behaviour;
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteeringCS.entity
+{
+
+    public abstract class MovingEntity : BaseGameEntity
+    {
+        public Vector2D Velocity { get; set; }
+        public float Mass { get; set; }
+        public float MaxSpeed { get; set; }
+
+        public SteeringBehaviour SB { get; set; }
+
+        public MovingEntity(Vector2D pos, World w) : base(pos, w)
+        {
+            Mass = 30;
+            MaxSpeed = 150;
+            Velocity = new Vector2D();
+        }
+
+        public override void Update(float timeElapsed)
+        {
+            // calculate the combined force from each steering behavior in the vehicle's list
+            //SB = new SeekBehaviour(this);
+            //SB = new FleeBehaviour(this);
+            SB = new ArriveBehaviour(this);
+            Vector2D steeringForce = SB.Calculate();
+
+            // acceleration = force / mass
+            Vector2D acceleration = steeringForce.divide(Mass);
+
+            // update velocity
+            Velocity.Add(acceleration.Multiply(timeElapsed));
+
+            // make sure vehicle does not exceed maximum velocity
+            Velocity.truncate(MaxSpeed);
+
+            // update the position
+            Pos = Pos.Add(Velocity.Multiply(timeElapsed));
+
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}", Velocity);
+        }
+    }
+}
---
+++ util/Vector2D.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteeringCS
+{
+   
+    public class Vector2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector2D() : this(0,0)
+        {
+        }
+
+        public Vector2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Length()
+        {
+            double xPow = Math.Pow(X, 2);
+            double yPow = Math.Pow(Y, 2);
+            return Math.Sqrt(xPow + yPow);
+        }
+
+        public double LengthSquared()
+        {
+            return Math.Sqrt(Length());
+        }
+
+        public Vector2D Add(Vector2D v)
+        {
+            this.X += v.X;
+            this.Y += v.Y;
+            return this;
+        }
+
+        public Vector2D Sub(Vector2D v)
+        {
+            this.X -= v.X;
+            this.Y -= v.Y;
+            return this;
+        }
+
+        public Vector2D Multiply(double value)
+        {
+            this.X *= value;
+            this.Y *= value;
+            return this;
+        }
+
+        public Vector2D divide(double value)
+        {
+            this.X = X / value;
+            this.Y = Y / value;
+            return this;
+        }
+
+        public Vector2D Normalize()
+        {
+            X = X / Length();
+            Y = Y / Length();
+            return this;
+        }
+
+        public Vector2D truncate(double maX)
+        {
+            if (Length() > maX)
+            {
+                Normalize();
+                Multiply(maX);
+            }
+            return this;
+        }
+        
+        public Vector2D Clone()
+        {
+            return new Vector2D(this.X, this.Y);
+        }
+        
+        public override string ToString()
+        {
+            return String.Format("({0},{1})", X, Y);
+        }
+    }
+
+
+}
